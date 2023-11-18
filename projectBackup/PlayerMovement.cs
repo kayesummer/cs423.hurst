23dbdd0cd64fbb6ebbef99677f79e6b89c026ef1
kayesummer <105686958+kayesummer@users.noreply.github.com>
2023-11-18 12:22:10 -0600

    added sprite flipping

--- projectBackup/PlayerMovement.cs
+++ projectBackup/PlayerMovement.cs
@@ -7,11 +7,20 @@ public class PlayerMovement : MonoBehaviour
      public Rigidbody2D playerRb;
      public float speed;
      public float input; //keeps track of x input (going left or right)
+     public SpriteRenderer spriteRenderer;
 
     // Update is called once per frame
     void Update()
     {
           input = Input.GetAxisRaw("Horizontal");
+          if (input < 0)
+          {
+               spriteRenderer.flipX = true;
+          }
+          else if (input > 0)
+          {
+               spriteRenderer.flipX = false;
+          }
     }
 
      void FixedUpdate()
