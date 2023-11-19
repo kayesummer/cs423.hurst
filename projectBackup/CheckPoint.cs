5168ed1f3f6a91f4f49a227da5675448fa24a8b7
kayesummer <105686958+kayesummer@users.noreply.github.com>
2023-11-18 20:20:49 -0600

    checkpoint

---
+++ projectBackup/CheckPoint.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+     private PlayerRespawn playerRespawn;
+     
+     
+     // Start is called before the first frame update
+    void Start()
+    {
+          playerRespawn = GameObject.Find("Player").GetComponent<PlayerRespawn>();   
+    }
+
+     // if player passes checkpoint, set new respawn point to the checkpoints location
+     private void OnTriggerEnter2D(Collider2D collision)
+     {
+        if (collision.gameObject.name == "Player")
+        {
+               playerRespawn.respawnPoint = transform.position;
+        }
+    }
+
+
+}
